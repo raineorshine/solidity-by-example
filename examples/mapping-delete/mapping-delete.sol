3151c450d06664b66fa088f7e4c2fa277ce44884
Raine Revere <raineorshine@gmail.com>
2016-07-21 13:28:12 -0600

    Add mapping-delete.

---
+++ examples/mapping-delete/mapping-delete.sol
@@ -0,0 +1,13 @@
+contract Contract {
+    struct Data {
+        uint a;
+        uint b;
+    }
+    mapping (uint => Data) public items;
+    function Contract() {
+        items[0] = Data(1,2);
+        items[1] = Data(3,4);
+        items[2] = Data(5,6);
+        delete items[1];
+    }
+}
