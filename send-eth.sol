3fb7cfa9e6710623e0d5ae66f14f9c2bc95bda6c
Raine Revere <raineorshine@gmail.com>
2017-01-27 20:09:34 -0700

    Add send-eth.

---
+++ send-eth.sol
@@ -0,0 +1,20 @@
+contract MyContract {
+
+    address a = 0x123;
+
+    function foo() {
+
+        // send ether with default 21,000 gas
+        // likely causes OOG in callee
+        a.send(1 ether);
+
+        // send ether with all remaining gas
+        // but no success check!
+        a.call.value(1 ether)();
+
+        // RECOMMENDED
+        // send all remaining gas
+        // explicitly handle callee throw
+        if(a.call.value(1 ether)()) throw;
+    }
+}
